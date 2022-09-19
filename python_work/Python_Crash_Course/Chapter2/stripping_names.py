f0d14ffbafeb23fb8bdf4606898820364fc854d8
Trinidad M Dena <deitychomp@yahoo.com>
2022-09-19 16:42:18 -0400

    stripping whitespace

---
+++ python_work/Python_Crash_Course/Chapter2/stripping_names.py
@@ -0,0 +1,19 @@
+name = "    Trinidad\n\t    Mario\n\t\t    Dena    "
+
+print(name)
+
+name = name.lstrip()
+
+print(name)
+
+name = name.rstrip()
+
+print(name)
+
+name = name.strip()
+
+print(name)
+
+
+
+
