4c8a4f0334616c646a34f7c77143bc5c5fc66de5
Trinidad M Dena <deitychomp@yahoo.com>
2022-09-24 07:52:22 -0400

    string concat with elements in array

---
+++ python_work/Python_Crash_Course/Chapter2/greetings.py
@@ -0,0 +1,6 @@
+names = ['Miguel' , 'Jesus' , 'Joshua' , 'Chino']
+
+print('Hello ' + names[0] + ', how are you?')
+print('Hello ' + names[1] + ', how are you?')
+print('Hello ' + names[2] + ', how are you?')
+print('Hello ' + names[3] + ', how are you?')
