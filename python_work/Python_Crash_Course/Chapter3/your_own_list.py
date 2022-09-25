ee810e5411c16a8ac4bc390387f30cb181da5e03
Trinidad M Dena <deitychomp@yahoo.com>
2022-09-25 10:03:50 -0400

    list of food

---
+++ python_work/Python_Crash_Course/Chapter3/your_own_list.py
@@ -0,0 +1,7 @@
+list = ['Rice' , 'Steak' , 'Fish' , 'Ramen']
+
+print('I like white ' + list[0])
+print('I like medium done ' + list[1])
+print('I like fried ' + list[2])
+print('I like tonkotsu ' + list[3])
+
