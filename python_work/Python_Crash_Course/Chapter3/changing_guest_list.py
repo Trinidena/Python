a226812428a2f8d7fa0e646d58daad524a31fdf5
Trinidad M Dena <deitychomp@yahoo.com>
2022-09-29 20:35:46 -0400

    modified list

---
+++ python_work/Python_Crash_Course/Chapter3/changing_guest_list.py
@@ -0,0 +1,21 @@
+guest_list = []
+print(guest_list)
+guest_list.append('Jesus')
+print(guest_list)
+guest_list.append('Edwin')
+print(guest_list)
+guest_list.append('Miguel')
+print(guest_list)
+
+print(guest_list[0] + ' is invited')
+print(guest_list[1] + ' is invited')
+print(guest_list[2] + ' is invited')
+
+print('Jesus can\'t make it')
+del guest_list[0]
+guest_list.insert(0 , 'Joshua')
+print(guest_list)
+
+print(guest_list[0] + ' is invited')
+print(guest_list[1] + ' is invited')
+print(guest_list[2] + ' %nis invited')
--- python_work/Python_Crash_Course/Chapter3/guest_list.py
+++ python_work/Python_Crash_Course/Chapter3/guest_list.py
@@ -6,3 +6,7 @@ guest_list.append('Edwin')
 print(guest_list)
 guest_list.append('Miguel')
 print(guest_list)
+
+print(guest_list[0] + ' is invited')
+print(guest_list[1] + ' is invited')
+print(guest_list[2] + ' is invited')
