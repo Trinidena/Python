652476f49372be3eef866f10e77ce3c2447a4aca
Trinidad M Dena <deitychomp@yahoo.com>
2022-10-05 22:14:47 -0400

    3.7

---
+++ python_work/Python_Crash_Course/Chapter3/shrinking_guest_list.py
@@ -0,0 +1,38 @@
+guest_list = []                 #Excercise 3.7
+print(guest_list)
+guest_list.append('Jesus')
+print(guest_list)
+guest_list.append('Edwin')
+print(guest_list)
+guest_list.append('Miguel')
+print(guest_list)
+
+print(guest_list[0] + ' is invited')
+print(guest_list[1] + ' is invited')
+print(guest_list[2] + ' is invited')
+
+print('Jesus can\'t make it')
+del guest_list[0]
+guest_list.insert(0 , 'Joshua')
+print(guest_list)
+
+print(guest_list[0] + ' is invited')
+print(guest_list[1] + ' is invited')
+print(guest_list[2] + ' is invited')
+
+print('I found a bigger dining table!')
+
+guest_list.insert(0 , 'Fabian')
+guest_list.insert(2, 'Isaac')
+guest_list.append('Ian')
+
+print(guest_list)
+
+print(guest_list[0] + ' is invited')
+print(guest_list[1] + ' is invited')
+print(guest_list[2] + ' is invited')
+print(guest_list[3] + ' is invited')
+print(guest_list[4] + ' is invited')
+print(guest_list[5] + ' is invited')
+
+print('I can only invite two people for dinner, the table is arriving late.')
